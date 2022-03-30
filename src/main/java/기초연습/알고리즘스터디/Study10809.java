66588da1a53a356bd8fc504e2dbca6727d5ad1b8
신동석 <96603945+dss1222@users.noreply.github.com>
2022-03-30 23:15:37 +0900

    문제추가

--- src/main/java/기초연습/알고리즘스터디/Study10809.java
+++ src/main/java/기초연습/알고리즘스터디/Study10809.java
@@ -1,4 +1,17 @@
 package 기초연습.알고리즘스터디;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Study10809 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println(A+B);
+        }
+
+    }
 }
