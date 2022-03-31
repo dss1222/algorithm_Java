898aeaca6331349c0704d740a34f04e21b4c0d84
신동석 <96603945+dss1222@users.noreply.github.com>
2022-03-31 15:52:03 +0900

    문제추가

--- src/main/java/기초연습/단계별로풀기/one차원배열/최소최대_10818.java
+++ src/main/java/기초연습/단계별로풀기/one차원배열/최소최대_10818.java
@@ -1,4 +1,26 @@
 package 기초연습.단계별로풀기.one차원배열;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class 최소최대_10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int index = 0;
+        int[] arr = new int[N];
+        while(st.hasMoreTokens()) {  //hashMoreTokens 는 토큰이 남아있으면 true 없으면 false를 반환
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N - 1]);
+    }
 }
