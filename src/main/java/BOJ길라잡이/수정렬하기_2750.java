5c6e0567236ab5e08f8459114835d330f32e4faa
신동석 <96603945+dss1222@users.noreply.github.com>
2022-04-01 19:38:04 +0900

    문제추가

---
+++ src/main/java/BOJ길라잡이/수정렬하기_2750.java
@@ -0,0 +1,21 @@
+package BOJ길라잡이;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 수정렬하기_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
