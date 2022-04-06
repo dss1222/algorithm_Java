2ed0bb27450e2897d444127a3499060d8970eaff
신동석 <96603945+dss1222@users.noreply.github.com>
2022-04-06 10:34:18 +0900

    문제추가

---
+++ src/main/java/기초연습/고양이.java
@@ -0,0 +1,10 @@
+package 기초연습;
+
+public class 고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
