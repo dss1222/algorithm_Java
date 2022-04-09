e6d1604bb31c139cdf07d8381449fff0f9a5ec19
신동석 <96603945+dss1222@users.noreply.github.com>
2022-04-09 09:36:17 +0900

    문제추가

---
+++ src/main/java/기초연습/단계별로풀기/브루트포스/덩치_7568.java
@@ -0,0 +1,40 @@
+package 기초연습.단계별로풀기.브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 덩치_7568 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+
+        String[] str;
+        for(int i = 0; i < N; i++) {
+            str = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(str[0]);	// [i][0] : 몸무게
+            arr[i][1] = Integer.parseInt(str[1]);	// [i][1] : 키
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            int rank = 1;
+
+            for(int j = 0; j < N; j++) {
+                if(i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+
+            sb.append(rank).append(' ');
+        }
+        System.out.println(sb);
+    }
+}
